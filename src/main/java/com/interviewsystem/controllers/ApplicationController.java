0f17c01661838cd173ac624618d7c14e77fa4e76
NishantKM <39297823+NishantKM@users.noreply.github.com>
2020-02-09 19:01:35 +0530

    Update ApplicationController.java

--- src/main/java/com/interviewsystem/controllers/ApplicationController.java
+++ src/main/java/com/interviewsystem/controllers/ApplicationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.interviewsystem.dao.InterviewerDaoService;
 import com.interviewsystem.models.Interviewer;
 
+/** main controller */
 @RestController
 public class ApplicationController {
 
